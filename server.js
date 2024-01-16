49be355f006680472ed2464d0e6d943a9a9ad983
Abdev23 <agadir.9ine98@gmail.com>
2024-01-16 02:36:07 +0100

    Fix: deploy the project to vercel

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 
 import express from 'express';
 import dotenv from 'dotenv';
+import rateLimiter from 'express-rate-limit';
+import cors from 'cors';
+import helmet from 'helmet';
+import 'express-async-errors';
 
 import route_app from './routes/route_app.js';
 import route_main from './routes/route_main.js';
@@ -10,6 +14,16 @@ const app = express();
 dotenv.config();
 
 app.set( 'view engine', 'ejs' );
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    max: 100,
+    windowMs: 15 * 60 * 1000,
+    message: 'Too many requests for this IP, please try again in an hour!'
+  })
+);
+app.use( cors() );
+app.use( helmet() );
 app.use( express.static('public') );
 app.use( express.json({limit: '10kb'}) );
 app.use( express.urlencoded({extended: false}) );
@@ -35,3 +49,6 @@ const start = async () => {
 };
 
 start();
+
+
+export default app;
